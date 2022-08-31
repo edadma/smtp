5b1f283fbc48f1a5992a097865c476ff9c69d6e0
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-30 20:16:10 -0400

    code Main

--- src/main/scala/io/github/edadma/smtp/Main.scala
+++ src/main/scala/io/github/edadma/smtp/Main.scala
@@ -10,5 +10,4 @@ import scala.concurrent.Future
 import scala.io.Codec
 import scala.util.{Failure, Success}
 
-@main def run(hostname: String, port: Int, flags: Int = 0, backlog: Int = 4096): Unit =
-  server(hostname, port, flags, backlog)
+@main def run(hostname: String, port: Int): Unit = server(hostname, port, 0, 4096)
